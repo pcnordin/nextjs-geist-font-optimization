75a3afbef9f8d5e7a757acc4f3209ee5d77dd0e7
pcnordin <pcnordin@outlook.com>
2025-05-25 20:46:48 +0100

    Add .next/static/chunks/src_app_page_tsx_0ef0cac3._.js 1748202407384

---
+++ .next/static/chunks/src_app_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_d2ed38d8._.js",
+  "static/chunks/src_4dd4ab6e._.js"
+],
+    source: "dynamic"
+});
